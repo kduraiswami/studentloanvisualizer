0927e1423851f346a1b1a560361f26993a88cf56
Kailash <kaidurai@gmail.com>
2015-07-26 18:07:37 -0700

    added initialize function, centered map around sf for fun

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+!function(){
+
+  var initializeMap = function(){
+    var center = new google.maps.LatLng(37.7580 , -122.4400);
+    var mapOptions = {zoom: 13, center: center};
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+}
+
+$(function(){
+  if (document.getElementById('map-canvas')) initializeMap();
 });
+
+}();
