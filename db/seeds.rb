7c6ef33e2a879eac3b3ee4aca084ef33bf35ff4b
Kailash <kaidurai@gmail.com>
2015-07-27 16:25:50 -0700

    changed file location to db/ from ./ to solve IO location error

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ require 'roo'
 require 'open-uri'
 require 'pp'
 
-excel_student_loan_portfolios = Roo::Spreadsheet.open('./loanportfoliodata.xlsx')
-excel_student_loan_portfolios = Roo::Excelx.new("./loanportfoliodata.xlsx", file_warning: :ignore)
+excel_student_loan_portfolios = Roo::Spreadsheet.open('db/loanportfoliodata.xlsx')
+excel_student_loan_portfolios = Roo::Excelx.new("db/loanportfoliodata.xlsx")
 
 raw_student_loan_portfolios = []
 
@@ -13,15 +13,15 @@ end
 
 raw_student_loan_portfolios.each do |individual_loan|
 	LoanPortfolio.create(
-		"institution_name" => individual_loan[1][2].value,
-		"address" => individual_loan[1][3].value,
-		"city" => individual_loan[1][4].value,
-		"St" => individual_loan[1][5].value,
-		"ZIP" => individual_loan[1][6].value,
-		"borrowers_in_repayment" => individual_loan[1][7].value,
-		"borrowers_in_default" => individual_loan[1][8].value,
-		"default_rate" => individual_loan[1][9].value,
-		"total_borrowers_default" => individual_loan[1][10].value,
-		"outstanding_principal" => individual_loan[1][11].value,
+		"institution_name" => individual_loan[2].value,
+		"address" => individual_loan[3].value,
+		"city" => individual_loan[4].value,
+		"St" => individual_loan[5].value,
+		"ZIP" => individual_loan[6].value,
+		"borrowers_in_repayment" => individual_loan[7].value,
+		"borrowers_in_default" => individual_loan[8].value,
+		"default_rate" => individual_loan[9].value,
+		"total_borrowers_default" => individual_loan[10].value,
+		"outstanding_principal" => individual_loan[11].value,
 		)
 end
