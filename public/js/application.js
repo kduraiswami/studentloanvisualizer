ef8c7eeb731e50c46f58afc56caf0cb1d716806b
Kailash <kaidurai@gmail.com>
2015-07-27 17:28:33 -0700

    added logic to start placing marker, realized I need to geocode these locations to properly post to google

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,23 @@
 
 !function(){
 
-  var initializeMap = function(){
-    var center = new google.maps.LatLng(37.7580 , -122.4400);
-    var mapOptions = {zoom: 13, center: center};
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	var initializeMap = function(){
+		var center = new google.maps.LatLng(39.50 ,  -98.35);
+		var mapOptions = {zoom: 5, center: center};
+		map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	}
 
-}
+	var markerLatLng = new google.maps.LatLng(39.50 ,  -98.35)
 
-$(function(){
-  if (document.getElementById('map-canvas')) initializeMap();
-});
+	var marker = new google.maps.Marker({
+		position: markerLatLng,
+		map: map,
+		title: ENV.loanPortfolio.institution_name
+	});
+
+
+	$(function(){
+		if (document.getElementById('map-canvas')) initializeMap();
+	});
 
 }();
