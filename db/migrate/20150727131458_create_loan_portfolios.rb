257ab5a93c4c20ac568e1c55ee640f55ff230fab
Kailash <kaidurai@gmail.com>
2015-07-27 19:43:41 -0700

    condense address into one field to combine street, city, state, zip

--- db/migrate/20150727131458_create_loan_portfolios.rb
+++ db/migrate/20150727131458_create_loan_portfolios.rb
@@ -3,9 +3,6 @@ class CreateLoanPortfolios < ActiveRecord::Migration
   	create_table :loan_portfolios do |t|
   		t.string "institution_name"
   		t.string "address"
-		t.string "city"
-		t.string "St"
-		t.string "ZIP"
 		t.string "borrowers_in_repayment"
 		t.string "borrowers_in_default"
 		t.string "default_rate"
