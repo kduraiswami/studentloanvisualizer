8c23cf99df3e602f1ffde1daebcd9dfd6bb34540
kduraiswami <kaidurai@gmail.com>
2016-12-11 14:13:01 -0800

    readability clean up

--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,50 @@
 
 	var initializeMap = function(){
 		
+		var mapStyling = 
+		[
+		{
+			"featureType":"water",
+			"elementType":"all",
+			"stylers":[{"hue":"#71d6ff"},{"saturation":100},{"lightness":-5},{"visibility":"on"}]
+		},
+		{
+			"featureType":"poi",
+			"elementType":"all",
+			"stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]
+		},
+		{
+			"featureType":"transit",
+			"elementType":"all",
+			"stylers":[{"hue":"#ffffff"},{"saturation":0},{"lightness":100},{"visibility":"off"}]
+		},
+		{
+			"featureType":"road.highway",
+			"elementType":"geometry",
+			"stylers":[{"hue":"#deecec"},{"saturation":-73},{"lightness":72},{"visibility":"on"}]
+		},
+		{
+			"featureType":"road.highway",
+			"elementType":"labels",
+			"stylers":[{"hue":"#bababa"},{"saturation":-100},{"lightness":25},{"visibility":"on"}]
+		},
+		{
+			"featureType":"landscape",
+			"elementType":"geometry",
+			"stylers":[{"hue":"#e3e3e3"},{"saturation":-100},{"lightness":0},{"visibility":"on"}]
+		},
+		{
+			"featureType":"road",
+			"elementType":"geometry",
+			"stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]
+		},
+		{
+			"featureType":"administrative",
+			"elementType":"labels",
+			"stylers":[{"hue":"#59cfff"},{"saturation":100},{"lightness":34},{"visibility":"on"}]
+		}
+		]
+
 		var center = new google.maps.LatLng(39.50 , -98.35);
 
 		var mapOptions = {
@@ -14,11 +58,8 @@
 			mapTypeID: 'Styled'
 		};
 
-		var styles = [{"featureType":"water","elementType":"all","stylers":[{"hue":"#71d6ff"},{"saturation":100},{"lightness":-5},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":0},{"lightness":100},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#deecec"},{"saturation":-73},{"lightness":72},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"hue":"#bababa"},{"saturation":-100},{"lightness":25},{"visibility":"on"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"hue":"#e3e3e3"},{"saturation":-100},{"lightness":0},{"visibility":"on"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"hue":"#59cfff"},{"saturation":100},{"lightness":34},{"visibility":"on"}]}]
 
-		var styledMap = new google.maps.StyledMapType(styles,
-			{name: "Styled Map"});
-
+		var styledMap = new google.maps.StyledMapType(mapStyling, {name: "Styled Map"});
 		map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 		map.mapTypes.set('map_style', styledMap);
